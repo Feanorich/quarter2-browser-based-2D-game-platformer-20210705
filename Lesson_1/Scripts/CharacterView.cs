8ec366b7f4ebef362804a13ee5db2450d88afe9c
Alexander <feanorich@yandex.ru>
2021-07-09 03:32:55 +0300

    Урок№1
    Платформа. 
    Персонаж, аниматор по методичке
    паралакс фона по методичке

---
+++ Lesson_1/Scripts/CharacterView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Platformer
+{
+    class CharacterView : MonoBehaviour
+    {
+        public SpriteRenderer SpriteRenderer;
+        //add other useful refs
+
+    }
+}
---
+++ Lesson_1/Scripts/Lessons.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Platformer
+{
+    public class Lessons : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private SpriteRenderer _back;
+        [SerializeField] private CharacterView _characterView;
+        //[SerializeField]
+        //private SomeView _someView;
+        //add links to test views <1>
+
+        //private SomeManager _someManager;
+        //add links to some logic managers <2>
+        private ParalaxManager _paralaxManager;
+        private SpriteAnimator _spriteAnimator;
+
+        private void Start()
+        {
+            _paralaxManager = new ParalaxManager(_camera.transform, _back.transform);
+
+            SpriteAnimationsConfig config = Resources.Load<SpriteAnimationsConfig>("SpriteAnimationsConfig");
+
+            _spriteAnimator = new SpriteAnimator(config);
+
+            _spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.sonic_walk, true, 10);
+
+        }
+
+        private void Update()
+        {
+            //_someManager.Update();
+            //update logic managers here <5>
+            _paralaxManager.Update();
+            _spriteAnimator.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            //_someManager.FixedUpdate();
+            //update logic managers here <6>
+        }
+
+        private void OnDestroy()
+        {
+            //_someManager.Dispose();
+            //dispose logic managers here <7>
+        }
+    }
+}
